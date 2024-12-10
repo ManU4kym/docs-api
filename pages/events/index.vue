656e29e426e6b5f0f4c872292002401e010771e0
ManU4kym <emmannjoroge36@gmail.com>
2024-12-10 22:06:17 +0300

    scene

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -1,12 +1,26 @@
 <template>
-    <div>
-        Events part
-    </div>
-</template>
+    <div ref="threeContainer" style="width: 100%; height: 100%"></div>
+  </template>
+  
+  <script setup>
+  import { ref, onMounted } from 'vue';
+  import { useThree } from '~/composables/useThree';
+  
 
-<script setup>
-</script>
-
-<style scoped>
-/* Styles here */
-</style>
+  const threeContainer = ref(null);
+  
+  const { init } = useThree();
+  
+  onMounted(() => {
+    if (threeContainer.value) {
+      init(threeContainer.value);
+    }
+  });
+  </script>
+  
+  <style scoped>
+  div {
+    display: block;
+  }
+  </style>
+  
