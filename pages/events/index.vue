bc34da47ae5121f0e52ff674af4da2d951cbd463
ManU4kym <emmannjoroge36@gmail.com>
2024-12-16 14:00:28 +0300

    /index

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="p-7">
-    <h1>Events Page</h1>
+    <h1>{{ name || 'Vendor' }}</h1>
     <div>
-      <h2>Products</h2>
+
       <div class="grid grid-cols-1 gap-5 m-8 sm:grid-cols-2 md:grid-cols-5 shadow-lg border p-5  tracking-normal ">
         <div v-for="product in products" :key="product.id" class="tracking-wide leading-6 sm:tracking-normal">
-          <nuxt-link :to="`/events/${product.id}`" class="hover:text-purple-700 ">
+          <nuxt-link :to="`/events/${product.id}`" class="hover:text-orange-950">
             {{ product.title }}
           </nuxt-link>
         </div>
@@ -16,6 +16,13 @@
 </template>
 
 <script setup>
+
+import { ref, onMounted } from 'vue';
+
+// State for products and name
+const jna = ref([]);
+const name = ref("");
 // Correct syntax for useFetch
 const { data: products } = await useFetch("https://fakestoreapi.com/products");
+
 </script>
