584e732a3cc9aa43b0cc1a2f1584523dfa48d0f8
ManU4kym <emmannjoroge36@gmail.com>
2024-12-16 13:13:21 +0300

    texturesz

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+    <div
+        class="bg-[url('assets/images/TCom_Ground_SandRoughSliding1_header2.jpg')] bg-contain bg-center h-screen text-black font-bold">
+        <header class="justify-between py-6 px-9 flex shadow-lg shadow-black    mx-auto  ">
+            <NuxtLink to="/" class="logo py-0 text-sm md:text-lg">Kimmiy</NuxtLink>
+
+            <nav class="grid grid-cols-4  gap-1 px-7 md:flex md:gap-4 text-sm md:text-base">
+                <NuxtLink to="/">Home</NuxtLink>
+                <NuxtLink to="/events">About</NuxtLink>
+                <NuxtLink to="/events/id">Work</NuxtLink>
+                <NuxtLink to="/contact">Contact</NuxtLink>
+            </nav>
+        </header>
+        <main class="dark:bg-grey-800">
+
+            <slot />
+        </main>
+    </div>
+</template>
+
+<style lang="css" scoped>
+.router-link-exact-active {
+    color: #f9f9f9;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,15 @@
-<!-- <script setup lang="ts">
-const counter = useState('counter', () => Math.round(Math.random() * 1000))
-</script>
-
 <template>
-  <div>
-    Counter: {{ counter }}
-    <button @click="counter++">
-      +
-    </button>
-    <button @click="counter--">
-      -
-    </button>
+  <div class="grid grid cols-1 border p-8 m-7 gap-2 tracking-tighter">
+    <div v-for="p in produce" :key="p.id" class=" text-sm ">
+      <p>{{ p.id }} : {{ p.title }}</p>
+    </div>
   </div>
-</template> -->
-
-
-<template>
-  <header class="justify-between py-6 px-9 flex shadow-md shadow-white    mx-auto  ">
-    <NuxtLink to="/" class="logo py-0 text-sm md:text-lg">Kimmiy</NuxtLink>
-
-    <nav class="grid grid-cols-4  gap-1 px-7 md:flex md:gap-4 text-sm md:text-base">
-      <NuxtLink to="/">Home</NuxtLink>
-      <NuxtLink to="/events">About</NuxtLink>
-      <NuxtLink to="/events/id">Work</NuxtLink>
-      <NuxtLink to="/contact">Contact</NuxtLink>
-    </nav>
-  </header>
 </template>
 
-<style lang="css" scoped>
-.router-link-exact-active {
-  color: green;
-}
-</style>
+<style lang="css" scoped></style>
+<script setup>
+
+const { data: produce } = useFetch("https://fakestoreapi.com/products/")
+
+
+</script>
