a9373bc34ffa3893f5e377d33b56e863b906b696
ManU4kym <emmannjoroge36@gmail.com>
2024-12-20 15:26:24 +0300

    product dets

--- pages/events/[id].vue
+++ pages/events/[id].vue
@@ -1,15 +1,21 @@
+<template>
+    <div>
+        <ProductDetails :product="product" />
+    </div>
+</template>
+
 <script setup>
+
+import ProductDetails from '~/components/ProductDetails.vue';
+
+
+
 const id = useRoute().params.id;
 
 const uri = 'https://fakestoreapi.com/products/' + id;
 
 const { data: product } = await useFetch(uri);
-</script>
-<template>
-    <div class="p-6 leading-9 tracking-wider  text-sm m-8 shadow-red-500 shadow-2xl md:w-1/4">
-        <p> Title :{{ product.title }}</p>
-        <p>Price : {{ product.price }}</p>
-        <p>Identifier :{{ product.id }}</p>
-    </div>
 
-</template>
+</script>
+
+<style scoped></style>
