2803fd3b37d522696930a03d7de65ac3eecd8b34
ManU4kym <emmannjoroge36@gmail.com>
2025-01-01 09:28:36 +0300

    global middleware

--- server/routes/hello.ts
+++ server/routes/hello.ts
@@ -1,6 +1,8 @@
 export default defineEventHandler( async (event) => {
+    
+    const { name } = getQuery(event)
     return {
        
-        message: 'hello kim'
+        message: `hello kim`
     }
 })
