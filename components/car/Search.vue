eaf36a260a5a822b5e66dda98dc399cc5748bd19
ManU4kym <emmannjoroge36@gmail.com>
2025-01-02 21:45:08 +0300

    individual head - dynamic

--- components/car/Search.vue
+++ components/car/Search.vue
@@ -3,7 +3,7 @@
     <div
         class="font-serif w-[1000px] text-2xl rounded-full bg-white flex justify-between overflow-hidden drop-shadow-2xl mx-auto">
         <input v-model="city" type="text" class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none"
-            placeholder="Search by city..." :class="cityError ? 'bg-red-600' : ''" />
+            placeholder="Search by city..." :class="cityError ? 'bg-pink-600' : 'bg-green-200'" />
         <button class="bg-sky-500 px-10 text-white" @click="handleSearch">Search</button>
     </div>
     <!-- HOME SEARCH BAR -->
